1b21031782b5758db92001ad43d0a7c2ca064ff6
MaddPaws <joshuatakri33@gmail.com>
2025-10-29 01:20:31 +0530

    only c code is added

---
+++ controller_private.h
@@ -0,0 +1,45 @@
+/*
+ * File: controller_private.h
+ *
+ * Code generated for Simulink model 'controller'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Wed Oct 29 00:56:01 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: STMicroelectronics->ST10/Super10
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef controller_private_h_
+#define controller_private_h_
+#include "rtwtypes.h"
+#include "controller_types.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmIsMajorTimeStep
+#define rtmIsMajorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MAJOR_TIME_STEP)
+#endif
+
+#ifndef rtmIsMinorTimeStep
+#define rtmIsMinorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MINOR_TIME_STEP)
+#endif
+
+#ifndef rtmSetTPtr
+#define rtmSetTPtr(rtm, val)           ((rtm)->Timing.t = (val))
+#endif
+
+/* private model entry point functions */
+extern void controller_derivatives(void);
+
+#endif                                 /* controller_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ controller_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: controller_types.h
+ *
+ * Code generated for Simulink model 'controller'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Wed Oct 29 00:56:01 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: STMicroelectronics->ST10/Super10
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef controller_types_h_
+#define controller_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_controller_T RT_MODEL_controller_T;
+
+#endif                                 /* controller_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
